03e82bec606be1f1ccae46525a9e7d4ae9d91c26
zeddo123 <m.drissi@protonmail.com>
2019-12-17 15:31:41 +0100

    update/bug fix

--- historical_clickstream.py
+++ historical_clickstream.py
@@ -13,9 +13,11 @@ user2 = ['/home', '/red', '/login', '/user', '/one', '/pink']
 You should return the following:
 ['/login', '/user', '/one']
 """
+# the first solution can't find sequences in the middle-right
+# O(2n)
 
-user1 = ['home','register','login', 'user', 'one', 'two']
-user2 = ['two','home','red','login','user','one']
+user1 = ['home', 'blabla', 'home', 'user', 'cake', 'two', 'one', 'father']
+user2 = ['father', 'home', 'user', 'nice', 'user', 'cake', 'two', 'one', 'blabla']
 
 user_min, user_max = (user1, user2) if len(user1) < len(user2) else (user2, user1)
 
@@ -52,4 +54,14 @@ while end > 0 and begin < len(user_min) - 1:
 	begin += 1
 	end -= 1
 
+print(buff)
+# ----------------------------------- Other solution O(n²) <
+buff = (0, None)
+end = len(user_min)
+for p, history in enumerate(user_min):
+	for i in reversed(range(p,end)):
+		test_list = user_min[p:i+1]
+		len_test = len(test_list)
+		if ''.join(test_list) in user_max_str and len_test > buff[0]:
+			buff = (len_test, test_list)
 print(buff)
